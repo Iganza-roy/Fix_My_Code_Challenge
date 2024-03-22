0d6b1d53c825e0bec4ee84e22672638297923e7c
Roy Iganza Kibata <iganzaroy55@gmail.com>
2024-03-22 02:42:57 -0700

    modified

--- 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -9,36 +9,48 @@
  *
  * Return: 1 on success, -1 on failure
  */
+
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * delete_dnodeint_at_index - Delete a node at a specific index from a list
+ *
+ * @head: A pointer to the first element of a list
+ * @index: The index of the node to delete
+ *
+ * Return: 1 on success, -1 on failure
+ */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-    dlistint_t *current = *head;
-    unsigned int i;
-
     if (*head == NULL)
-    {
         return (-1);
-    }
+
+    dlistint_t *current = *head;
 
     if (index == 0)
     {
         *head = (*head)->next;
-        if (*head != NULL)
+        if (*head)
             (*head)->prev = NULL;
         free(current);
         return (1);
     }
 
-    for (i = 0; current != NULL && i < index; i++)
+    unsigned int i = 0;
+    while (i < index && current->next != NULL)
+    {
         current = current->next;
+        i++;
+    }
 
-    if (current == NULL)
+    if (i != index)
         return (-1);
 
     current->prev->next = current->next;
-    if (current->next != NULL)
+    if (current->next)
         current->next->prev = current->prev;
-    free(current);
 
+    free(current);
     return (1);
 }
-
