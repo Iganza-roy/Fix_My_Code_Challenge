72dbe22e57e7347b3aa658301889c60a6a6581e4
Roy Iganza Kibata <iganzaroy55@gmail.com>
2024-05-12 12:41:17 -0700

    Add Square.py

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 
-class square():
+class Square():
     
     width = 0
     height = 0
-
     
-    def __init__(self, *args, **kwargs):
+    def __init__(self, **kwargs):
         """init method"""
         for key, value in kwargs.items():
             setattr(self, key, value)
@@ -15,7 +14,7 @@ class square():
         """ Area of the square """
         return self.width * self.height
 
-    def PermiterOfMySquare(self):
+    def PerimiterOfMySquare(self):
         """perimeter of square"""
         return (self.width * 2) + (self.height * 2)
 
@@ -25,7 +24,7 @@ class square():
 
 if __name__ == "__main__":
 
-    s = square(width=12, height=9)
+    s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.PermiterOfMySquare())
+    print(s.PerimiterOfMySquare())
